364312f26f0c2422b650d37298d0b0aae0c33a46
RafaelGini <98921181+RafaelGini@users.noreply.github.com>
2024-05-22 20:54:02 -0300

    Primer Splash Listo

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -16,6 +17,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import {
   Colors,
@@ -56,6 +58,14 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    if (Platform.OS === 'android'){
+      SplashScreen.hide();
+    }
+  }, [])
+
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
