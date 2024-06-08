d446b9bc8b0b2bf3b81b57c36c0b13192801b69a
RafaelGini <98921181+RafaelGini@users.noreply.github.com>
2024-06-08 17:57:41 -0300

    GOOGLE SIGN IN FUNCIONANDO

--- app/ui/screens/LoginScreens/LoginScreen.jsx
+++ app/ui/screens/LoginScreens/LoginScreen.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import theme from '../../styles/theme';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-community/google-signin';
+
+GoogleSignin.configure({
+  webClientId: '339637593763-19rqqksc5a7u595uiu4gl1pcer0qd383.apps.googleusercontent.com',
+  offlineAccess: true
+});
 
 const LoginScreen = ({ navigation }) => {
+
   const handleLogin = () => {
     navigation.replace('HomeTabs');
   };
+
+  const signIn = async () => {
+
+    await GoogleSignin.hasPlayServices();
+    console.log("iniciando sesion...");
+    const userInfo = await GoogleSignin.signIn();
+    console.log(userInfo);
+
+    // Aquí puedes hacer la llamada a tu API backend
+  };
+
 
   return (
     <View style={styles.container}>
       <Image source={require('../../../assets/images/MovieFinder_logo.png')} style={styles.logo} />
       <Text style={styles.title}>MovieFinder</Text>
       <Text style={styles.subtitle}>Ingrese con su cuenta de Google</Text>
-      <Button title="Login" onPress={handleLogin} />
+      <GoogleSigninButton
+        style={{ width: 192, height: 48 }}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+      />
     </View>
   );
 };
