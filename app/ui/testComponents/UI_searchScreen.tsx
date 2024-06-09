f799ef208620d991b2151552b149ed08a6b6f764
RafaelGini <98921181+RafaelGini@users.noreply.github.com>
2024-06-09 14:29:00 -0300

    Cambio en Search Screen

--- app/ui/testComponents/UI_searchScreen.tsx
+++ app/ui/testComponents/UI_searchScreen.tsx
@@ -1,22 +1,78 @@
+// SearchScreenUI.jsx
 import React from 'react';
-import { FlatList, View, Text, TextInput, StyleSheet } from 'react-native';
+import { FlatList, View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import Movie from '../../interfaces/Movie';
+import theme from '../styles/theme';
 
 interface SearchScreenUIProps {
   movies: Movie[];
   searchInput: string;
   setSearchInput: React.Dispatch<React.SetStateAction<string>>;
+  handleFilter: (filter: 'date' | 'rating' | 'default') => void;
+  toggleSorter: () => void;
+  currentFilter: 'date' | 'rating' | 'default';
+  currentSorter: 'asc' | 'desc';
 }
 
-const SearchScreenUI: React.FC<SearchScreenUIProps> = ({ movies, searchInput, setSearchInput }) => {
+const SearchScreenUI: React.FC<SearchScreenUIProps> = ({
+  movies,
+  searchInput,
+  setSearchInput,
+  handleFilter,
+  toggleSorter,
+  currentFilter,
+  currentSorter,
+}) => {
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
-        style={styles.input}
+        style={[styles.input, styles.textInput]} // Aplicar clase de estilo para el TextInput
         placeholder="Search movies..."
+        placeholderTextColor={theme.colors.text_light}
         value={searchInput}
         onChangeText={setSearchInput}
       />
+      <View style={styles.filterContainer}>
+        <TouchableOpacity
+          style={[
+            styles.filterButton,
+            currentFilter === 'date' && styles.activeFilter,
+            currentFilter === 'date' && styles.dateFilter,
+          ]}
+          onPress={() => handleFilter('date')}
+        >
+          <Text style={[styles.filterText, currentFilter === 'date' ? styles.activeText : styles.inactiveText]}>Date</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.filterButton,
+            currentFilter === 'rating' && styles.activeFilter,
+            currentFilter === 'rating' && styles.ratingFilter,
+          ]}
+          onPress={() => handleFilter('rating')}
+        >
+          <Text style={[styles.filterText, currentFilter === 'rating' ? styles.activeText : styles.inactiveText]}>Rating</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.filterButton,
+            currentFilter === 'default' && styles.activeFilter,
+            currentFilter === 'default' && styles.defaultFilter,
+          ]}
+          onPress={() => handleFilter('default')}
+        >
+          <Text style={[styles.filterText, currentFilter === 'default' ? styles.activeText : styles.inactiveText]}>Default</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.sortButton,
+            styles.sortButtonBg,
+          ]}
+          onPress={toggleSorter}
+        >
+          <Text style={[styles.filterText, styles.sortButtonText]}>{currentSorter === 'asc' ? 'Ascending' : 'Descending'}</Text>
+        </TouchableOpacity>
+      </View>
       <FlatList
         data={movies}
         keyExtractor={(item) => item.id.toString()}
@@ -31,12 +87,63 @@ const SearchScreenUI: React.FC<SearchScreenUIProps> = ({ movies, searchInput, se
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background
+  },
   input: {
     height: 40,
+    marginBottom: 16,
+  },
+  textInput: {
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 8,
+    backgroundColor: theme.colors.background,
+    borderRadius: 10,
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     marginBottom: 16,
+  },
+  filterButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 10,
+    backgroundColor: theme.colors.background,
+  },
+  activeFilter: {
+    backgroundColor: theme.colors.background_soft,
+  },
+  filterText: {
+    fontSize: 16,
+  },
+  activeText: {
+    color: theme.colors.primary,
+  },
+  inactiveText: {
+    color: '#ffffff', // Color del texto para botones no seleccionados
+  },
+  dateFilter: {
+    backgroundColor: theme.colors.background_soft,
+  },
+  ratingFilter: {
+    backgroundColor: theme.colors.background_soft,
+  },
+  defaultFilter: {
+    backgroundColor: theme.colors.background_soft,
+  },
+  sortButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 10,
+  },
+  sortButtonBg: {
+    backgroundColor: theme.colors.background_soft,
+  },
+  sortButtonText: {
+    color: theme.colors.primary,
   },
   movieItem: {
     padding: 8,
--- app/ui/testComponents/searchScreen.tsx
+++ app/ui/testComponents/searchScreen.tsx
@@ -1,64 +1,92 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, StyleSheet, FlatList, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { getMovies } from '../../services/searchMovies';
 import Movie from '../../interfaces/Movie';
 import SearchScreenUI from './UI_searchScreen';
+import theme from '../styles/theme';
 
 const SearchScreen = () => {
-    const [searchInput, setSearchInput] = useState<string>('');
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
-    useEffect(() => {
-      if (timer) {
-        clearTimeout(timer);
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+  const [filter, setFilter] = useState<'date' | 'rating' | 'default'>('default');
+  const [sorter, setSorter] = useState<'asc' | 'desc'>('desc');
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const newTimer = setTimeout(async () => {
+      if (searchInput.trim().length > 0) {
+        console.log("Se llamó al endpoint")
+        const fetchedMovies = await getMovies(searchInput);
+        setMovies(fetchedMovies);
       }
-      const newTimer = setTimeout(async () => {
-        if (searchInput.trim().length > 0) {
-            console.log("Se llamo al endpoint")
-          const fetchedMovies = await getMovies(searchInput);
-          setMovies(fetchedMovies);
+    }, 2000);
+    setTimer(newTimer);
+    return () => {
+      if (newTimer) {
+        clearTimeout(newTimer);
+      }
+    };
+  }, [searchInput]);
+
+  useEffect(() => {
+    // Apply sorting based on filter and sorter
+    let sortedMovies = [...movies];
+    if (filter === 'date') {
+      sortedMovies.sort((a, b) => {
+        if (sorter === 'asc') {
+          return new Date(a.release_date).getTime() - new Date(b.release_date).getTime();
+        } else {
+          return new Date(b.release_date).getTime() - new Date(a.release_date).getTime();
         }
-      }, 2000);
-      setTimer(newTimer);
-      return () => {
-        if (newTimer) {
-          clearTimeout(newTimer);
+      });
+    } else if (filter === 'rating') {
+      sortedMovies.sort((a, b) => {
+        if (sorter === 'asc') {
+          return a.vote_average - b.vote_average;
+        } else {
+          return b.vote_average - a.vote_average;
         }
-      };
-    }, [searchInput]);
-  
-    return (
-        <View style={styles.container}>
-        <SearchScreenUI
-          movies={movies}
-          searchInput={searchInput}
-          setSearchInput={setSearchInput}
-        />
-      </View>
-    );
+      });
+    }
+    setMovieList(sortedMovies);
+  }, [movies, filter, sorter]);
+
+  const handleFilter = (selectedFilter: 'date' | 'rating' | 'default') => {
+    setFilter(selectedFilter);
+    if (selectedFilter === 'default') {
+      setSorter('desc'); // Reset sorter to default if filter is default
+    }
   };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-      backgroundColor: '#fff',
-    },
-    input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      paddingHorizontal: 8,
-      marginBottom: 16,
-    },
-    movieItem: {
-      padding: 8,
-      borderBottomColor: '#ccc',
-      borderBottomWidth: 1,
-    },
-    movieTitle: {
-      fontSize: 18,
-    },
-  });
-  
-  export default SearchScreen;
+
+  const toggleSorter = () => {
+    setSorter(sorter === 'asc' ? 'desc' : 'asc');
+  };
+
+  return (
+    <View style={styles.container}>
+      <SearchScreenUI
+        movies={movieList}
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+        handleFilter={handleFilter}
+        toggleSorter={toggleSorter}
+        currentFilter={filter}
+        currentSorter={sorter}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: theme.colors.background
+  },
+});
+
+export default SearchScreen;
