79288a0c32ab91b01f6054f484b4d8267621e44b
marifortu45 <marianorossi2000@gmail.com>
2024-06-10 00:20:18 -0300

    imagePick

--- app/ui/screens/profile_screens/ProfileScreen.tsx
+++ app/ui/screens/profile_screens/ProfileScreen.tsx
@@ -3,6 +3,7 @@ import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity } from 'reac
 import theme from '../../styles/theme';
 import checkConnection from '../../../utils/checkConnection';
 import noInternetScreen from '../../../utils/noInternetScreen';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 const ProfileScreen = () => {
   const profile = {
@@ -15,9 +16,28 @@ const ProfileScreen = () => {
   }
 
   const [nickname, setNickname] = useState(profile.nickname);
+  const [image, setImage] = useState(profile.profileImage);
 
   const handleChangePic = () => {
     // Lógica para guardar los cambios en el servidor
+    const options = {
+      mediaType: 'photo',
+      includeBase64: false,
+      maxHeight: 2000,
+      maxWidth: 2000,
+    };
+  
+    launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('Image picker error: ', response.error);
+      } else {
+        let imageUri = response.uri || response.assets?.[0]?.uri;
+        setImage(imageUri);
+        //Post nueva imagen perfil
+      }
+    });
     console.log('Cambiar foto');
   };
 
@@ -54,7 +74,7 @@ const ProfileScreen = () => {
       <View style={styles.avatarContainer}>
         <Image
           style={styles.avatar}
-          source={{uri: profile.profileImage}}
+          source={{uri: image}}
         />
         <TouchableOpacity style={styles.changeAvatarButton} onPress={handleChangePic}>
           <Text style={styles.changeAvatarButtonText}>Cambiar foto de perfil</Text>
